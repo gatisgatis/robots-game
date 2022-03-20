bf40b35024b2326a9b74b05c90db4dafba42e184
Gatis Meikulis <gmeikulis@evolution.com>
2022-03-20 13:37:06 +0200

    basic ui structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import "./App.css"
 
 function App() {
-  return (
-    <div className="title">
-      <h1>Robots Game</h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="content-wrapper">
+                <h1>Robots Game</h1>
+                <h4>Enter your name and smash the Enter to calculate your super secret robot name</h4>
+                <input type="text" placeholder="Your name"/>
+                <div className="output">
+                    output here
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
